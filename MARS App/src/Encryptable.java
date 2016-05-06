93617dd27f4ea646fb085c23874b2ba7097ebf4f
yan-tao <geniusguyyan@gmail.com>
2016-05-06 14:18:34 -0700

    Created some classes/interfaces

---
+++ MARS App/src/Encryptable.java
@@ -0,0 +1,6 @@
+
+public interface Encryptable {
+	
+	String encrypt(String s);
+	String decrypt(String s);
+}
---
+++ MARS App/src/Receive.java
@@ -0,0 +1,16 @@
+
+public class Receive implements Encryptable {
+
+	@Override
+	public String encrypt(String s) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String decrypt(String s) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
